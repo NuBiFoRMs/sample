d235e8177e9a9799dd16b4f3650602335dbfd372
NuBiFoRM <nubiform@me.com>
2021-04-18 01:46:31 +0900

    init project

---
+++ spring-security-rest-login/src/main/java/com/nubiform/login/SpringSecurityRestLoginApplication.java
@@ -0,0 +1,13 @@
+package com.nubiform.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRestLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityRestLoginApplication.class, args);
+    }
+
+}
---
+++ spring-security-rest-login/src/test/java/com/nubiform/login/SpringSecurityRestLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nubiform.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRestLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
