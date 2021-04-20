090b0fb5b49586b0556c6d44444f33fcfa6847bf
NuBiFoRM <nubiform@me.com>
2021-04-20 14:04:15 +0900

    lombok logging

--- spirng-ioc-di/src/main/java/com/nubiform/spirngiocdi/App.java
+++ spirng-ioc-di/src/main/java/com/nubiform/spirngiocdi/App.java
@@ -1,13 +1,20 @@
 package com.nubiform.spirngiocdi;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class App {
+public class App implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.debug("application");
+    }
 }
