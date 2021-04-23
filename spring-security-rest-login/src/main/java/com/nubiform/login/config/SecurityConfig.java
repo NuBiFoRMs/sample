674fa1b3ac80bc1eba8481640bc11508cf610fec
NuBiFoRM <nubiform@me.com>
2021-04-23 15:07:25 +0900

    security config

--- spring-security-rest-login/src/main/java/com/nubiform/login/config/SecurityConfig.java
+++ spring-security-rest-login/src/main/java/com/nubiform/login/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
@@ -17,13 +18,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final LogoutSuccessHandler logoutSuccessHandler;
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/h2-console/**")
+                .antMatchers("/v3/api-docs/**")
+                .antMatchers("/swagger-ui.html", "/swagger-ui/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .cors().disable()
 //                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 //                .and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/sign-up", "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .logout()
