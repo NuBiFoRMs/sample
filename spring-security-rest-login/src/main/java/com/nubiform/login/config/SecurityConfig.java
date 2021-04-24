a56671145c36e5852b94eeee3b1ed864feee410a
NuBiFoRM <nubiform@me.com>
2021-04-24 09:58:32 +0900

    remove logout handler

--- spring-security-rest-login/src/main/java/com/nubiform/login/config/SecurityConfig.java
+++ spring-security-rest-login/src/main/java/com/nubiform/login/config/SecurityConfig.java
@@ -8,14 +8,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final LogoutSuccessHandler logoutSuccessHandler;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -29,14 +27,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .cors().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                 .antMatchers("/sign-up", "/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .logout()
-                .logoutSuccessHandler(logoutSuccessHandler);
+                .anyRequest().authenticated();
     }
 
     @Bean
