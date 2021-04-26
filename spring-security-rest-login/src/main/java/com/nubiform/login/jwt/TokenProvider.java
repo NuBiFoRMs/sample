3a917b1423548ebc98bc2aaa4768427d4fcee597
NuBiFoRM <nubiform@me.com>
2021-04-26 21:36:40 +0900

    token provider

--- spring-security-rest-login/src/main/java/com/nubiform/login/jwt/TokenProvider.java
+++ spring-security-rest-login/src/main/java/com/nubiform/login/jwt/TokenProvider.java
@@ -1,8 +1,9 @@
 package com.nubiform.login.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +14,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
@@ -23,8 +25,12 @@ import java.util.stream.Collectors;
 public class TokenProvider implements InitializingBean {
 
     public static final String AUTHORITIES_KEY = "auth";
+    public static final String DELIMITER = ",";
+
     private final String secret;
     private final long tokenValidityInMillisecond;
+
+    private Key key;
 
     public TokenProvider(@Value("${jwt.secret}") String secret,
                          @Value("${jwt.token-validity-in-second}") long tokenValidityInSecond) {
@@ -35,29 +41,33 @@ public class TokenProvider implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        log.debug("afterPropertiesSet");
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
     }
 
-    public String getToken(Authentication authentication) {
+    public String createToken(Authentication authentication) {
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
+                .collect(Collectors.joining(DELIMITER));
 
         Date now = new Date();
 
         return Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS512)
                 .setSubject(authentication.getName())
                 .claim(AUTHORITIES_KEY, authorities)
-                .signWith(SignatureAlgorithm.HS512, secret)
                 .setIssuedAt(now)
                 .setExpiration(new Date(now.getTime() + tokenValidityInMillisecond))
                 .compact();
     }
 
     public Authentication getAuthentication(String token) {
-        Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
 
-        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(DELIMITER))
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
 
@@ -66,11 +76,20 @@ public class TokenProvider implements InitializingBean {
         return new UsernamePasswordAuthenticationToken(principal, token, authorities);
     }
 
-    public String parseToken(String token) {
-        return Jwts.parser().setSigningKey(secret).parse(token).toString();
-    }
-
     public boolean validateToken(String token) {
-        return true;
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰 입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰 입니다.");
+        } catch (MalformedJwtException e) {
+            log.info("잘못된 JWT 토큰 입니다.");
+        } catch (SignatureException e) {
+            log.info("잘못된 JWT 서명 입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("잘못된 토큰 입니다.");
+        }
+        return false;
     }
 }
