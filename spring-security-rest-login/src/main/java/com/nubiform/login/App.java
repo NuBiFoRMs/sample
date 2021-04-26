42740a39a4216b1b268c1b3a55cf694ad61998f7
NuBiFoRM <nubiform@me.com>
2021-04-27 01:17:56 +0900

    secret key

--- spring-security-rest-login/src/main/java/com/nubiform/login/App.java
+++ spring-security-rest-login/src/main/java/com/nubiform/login/App.java
@@ -1,13 +1,26 @@
 package com.nubiform.login;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Base64;
+
+@Slf4j
 @SpringBootApplication
-public class App {
+public class App implements CommandLineRunner {
+
+    @Value("${jwt.secret}")
+    private String secret;
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.debug("{}", new String(Base64.getDecoder().decode(secret)));
+    }
 }
