73ee35ac6e0cc911439f32b4f34e533d40930bfa
NuBiFoRM <nubiform@me.com>
2021-04-26 21:47:19 +0900

    JwtAuthenticationFilter

--- spring-security-rest-login/src/main/java/com/nubiform/login/jwt/JwtAuthenticationFilter.java
+++ spring-security-rest-login/src/main/java/com/nubiform/login/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.nubiform.login.jwt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -8,9 +9,12 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import java.io.IOException;
 
+@Slf4j
 public class JwtAuthenticationFilter implements GenericFilterBean {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.debug("JwtAuthenticationFilter: doFilter");
 
+        chain.doFilter(request, response);
     }
 }
