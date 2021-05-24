8a75ad586e992f301fc41be7f2fa7e8a5738a409
NuBiFoRM <nubiform@me.com>
2021-05-24 20:10:16 +0900

    redis config

--- spring-redis-session/src/main/java/com/nubiform/session/RedisConfig.java
+++ spring-redis-session/src/main/java/com/nubiform/session/RedisConfig.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @RequiredArgsConstructor
 @Configuration
@@ -19,9 +16,4 @@ public class RedisConfig {
     RedisConnectionFactory lettuceConnectionFactory() {
         return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
     }
-
-//    @Bean
-//    RedisConnectionFactory jedisConnectionFactory() {
-//        return new JedisConnectionFactory(new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort()));
-//    }
 }
