271e298f68f2edaf2bbb6cd5a7c27b2b3732a5bd
NuBiFoRM <nubiform@me.com>
2021-05-24 19:33:35 +0900

    redis config

--- spring-redis-session/src/main/java/com/nubiform/session/RedisConfig.java
+++ spring-redis-session/src/main/java/com/nubiform/session/RedisConfig.java
@@ -1,7 +1,27 @@
 package com.nubiform.session;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+@RequiredArgsConstructor
 @Configuration
 public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    RedisConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+//    @Bean
+//    RedisConnectionFactory jedisConnectionFactory() {
+//        return new JedisConnectionFactory(new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort()));
+//    }
 }
---
+++ spring-redis-session/src/main/java/com/nubiform/session/RedisProperties.java
@@ -0,0 +1,15 @@
+package com.nubiform.session;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "spring.redis")
+public class RedisProperties {
+
+    private String host;
+
+    private int port;
+}
