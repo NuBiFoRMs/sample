588af6d79b3ef3102abb7f8d1b09aef987f6a5e3
NuBiFoRM <nubiform@me.com>
2021-06-07 22:53:53 +0900

    validator

---
+++ spring-web/src/main/java/com/nubiform/web/EventId.java
@@ -0,0 +1,24 @@
+package com.nubiform.web;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = EventIdValidator.class)
+@Documented
+public @interface EventId {
+
+    String message() default "{invalid.id}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
+
