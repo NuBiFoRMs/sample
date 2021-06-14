d58975c23166c63ab7c0026d1417840b87193bfa
NuBiFoRM <nubiform@me.com>
2021-06-14 18:13:46 +0900

    spring-web

--- spring-web/src/main/java/com/nubiform/web/EventController.java
+++ spring-web/src/main/java/com/nubiform/web/EventController.java
@@ -2,20 +2,13 @@ package com.nubiform.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@Validated
 public class EventController {
 
     private final EventService eventService;
