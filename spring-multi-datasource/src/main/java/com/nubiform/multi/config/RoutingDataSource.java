6ecea458a166a2a055c2bf20ae488ba755ecc7c3
NuBiFoRM <nubiform@me.com>
2021-06-16 18:18:14 +0900

    RoutingDatasource

--- spring-multi-datasource/src/main/java/com/nubiform/multi/config/RoutingDataSource.java
+++ spring-multi-datasource/src/main/java/com/nubiform/multi/config/RoutingDataSource.java
@@ -1,11 +1,15 @@
 package com.nubiform.multi.config;
 
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 public class RoutingDataSource extends AbstractRoutingDataSource {
     
     @Override
     protected Object determineCurrentLookupKey() {
-        return null;
+        if (TransactionSynchronizationManager.isCurrentTransactionReadOnly())
+            return "readOnly";
+        else
+            return "write";
     }
 }
